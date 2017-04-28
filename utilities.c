2a931c9283164f95167c5ad230f1063797ed7d48
DarthDaveeous <DarthDaveeous@gmail.com>
2017-04-28 16:20:05 -0700

    user4 feature implemented successfully

--- utilities.c
+++ utilities.c
@@ -1,5 +1,7 @@
 #include "utilities.h"
 
+const char *ServerTerminationMessage = "echo_s is stopping";
+
 const char *getIPAddress(struct sockaddr_in *clientAddress) {
   char *buffer = malloc(INET_ADDRSTRLEN * sizeof(char));
   inet_ntop(AF_INET, &clientAddress->sin_addr, buffer, INET_ADDRSTRLEN);
